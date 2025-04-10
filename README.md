0f98b862ca0804d3923cda387c5173b7f8d2538c
Richard Cassegrain-Lasierra <rcassegrainlasierra@gmail.com>
2025-04-10 17:10:54 +0200

    Cosmetic modifications

---
+++ README.md
@@ -0,0 +1,6 @@
+Lien Heroku : https://testcw-5443396f6b93.herokuapp.com/index.htm
+1) Sélectionner le fichier texte fourni par André
+2) Entrer le texte écouté dans la zonbe de saisie
+3) Cliquer sur le bouton "Résultast" pour afficher le nombre de fautes
+
+Bon courage F4LUC !
--- script.js
+++ script.js
@@ -83,3 +83,34 @@ function onbtnResultClick() {
         }
     }
 }
+
+var options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+var date = new Date();
+var datedisplay = date.toLocaleDateString("fr-FR", options);
+var hour = date.getUTCHours();
+var minute = date.getUTCMinutes();
+var second = date.getUTCSeconds();
+var timeDisplay = function() {
+    if(second<59)
+        second++;
+    else {
+        minute++;
+        second = 0;
+    }
+    if(minute>59) {
+        hour++;
+        minute = 0;
+    }
+    document.getElementById("horloge").textContent = datedisplay + " - " +  formatTime(hour) + ":" + formatTime(minute) + ":" + formatTime(second) + " UTC(Z)";
+    setTimeout(timeDisplay, 1000);
+}
+
+function formatTime(timeconvert) {
+    if(timeconvert.toString().length==2) {
+        return timeconvert.toString();
+    } else {
+        return "0" + timeconvert.toString();
+    }
+}
+
+setTimeout(timeDisplay, 1000);
